8dcfb281ef979ccf90dcdf476a668715ff6cc1e0
annezhou <azhou997@gmail.com>
2016-11-30 19:45:12 -0800

    uhh

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
       var answer = faqElement.find('.answer');
       if (!answer.hasClass('activeanswer')) {
         question.addClass('flipButton');
-        answer.css('max-height', 'none');
-        answer.css('max-height', answer.height());
+        // answer.css('max-height', 'none');
+        // answer.css('max-height', answer.css("height"));
         answer.addClass('activeanswer');
       }
       else if (answer.hasClass('activeanswer')) {
         question.removeClass('flipButton');
-        answer.css('max-height', 0);
+        // answer.css('max-height', 0);
         answer.removeClass('activeanswer');
       }
   });
